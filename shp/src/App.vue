de2721d6bf157056a697d368e1a81db943d024fd
Shoumq <lae345@mail.ru>
2022-12-14 19:57:56 +0300

    vse sdelal

--- shp/src/App.vue
+++ shp/src/App.vue
@@ -1,15 +1,41 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>>
+  <UserCard :email="emailApi" :phone="phoneApi" :FIO="FIOApi" :adress="adressApi" :imgUrl="imgUrlApi" :nick="nickApi"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserCard from './components/UserCard.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      emailApi: '',
+      phoneApi: '',
+      FIOApi: '',
+      nickApi: '',
+      adressApi: '',
+      imgUrlApi: '',
+    }
+  },
   components: {
-    HelloWorld
+    UserCard
+  },
+  methods: {
+    getUserData() {
+      axios.get('https://randomuser.me/api/').then((response) => {
+        console.log(response.data.results[0])
+        this.emailApi = response.data.results[0].email
+        this.phoneApi = response.data.results[0].phone
+        this.FIOApi = response.data.results[0].name.first + ' ' + response.data.results[0].name.last + ' ' + response.data.results[0].name.title;
+        this.adressApi = response.data.results[0].location.city + ', ' + response.data.results[0].location.state
+        this.imgUrlApi = response.data.results[0].picture.large,
+        this.nick = response.data.results[0].id.name
+      })
+    }
+  },
+  mounted() {
+    this.getUserData();
   }
 }
 </script>
---
+++ shp/src/components/UserCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="user-card">
+    <img :src="imgUrl" class="user-photo">
+    <p class="user-nickname">{{ nick }}</p>
+    <p class="user-name">
+       {{ FIO }}
+    </p>
+    <hr>
+    <p class="user-info-item">
+      <img src="../assets/img/location.png">
+      {{ adress }}
+    </p>
+    <p class="user-info-item">
+      <img src="../assets/img/mail.png">
+      {{ email }}
+    </p>
+    <p class="user-info-item">
+      <img src="../assets/img/phone.png">
+      {{ phone }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    imgUrl: String,
+    nick: String,
+    FIO: String,
+    adress: String,
+    email: String,
+    phone: String
+  }
+}
+</script>
+
+<style scoped>
+.user-card {
+  width: 300px;
+  padding: 20px;
+  border-radius: 10px;
+  margin-top: 100px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  box-shadow: 0 0 15px 0 rgba(50, 50, 50, 0.2);
+  text-align: center;
+}
+
+.user-card .user-photo {
+  box-sizing: border-box;
+  height: 150px;
+  margin-top: -100px;
+  border-radius: 50%;
+  border: 3px solid #ccc;
+}
+
+.user-card .user-name {
+  font-size: 30px;
+  font-weight: 900;
+  line-height: 1.4;
+  margin: 5px 0;
+}
+
+.user-card .user-nickname {
+  color: #a0a9b3;
+  font-size: 18px;
+  margin: 5px 0;
+  font-family: monospace;
+}
+
+.user-card hr {
+  height: 4px;
+  width: 50px;
+  margin: 10px auto;
+  background-color: #2c3e50;
+}
+
+.user-card .user-info-item img {
+  vertical-align: middle;
+  margin-right: 5px;
+}
+</style>
